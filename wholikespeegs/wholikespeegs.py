94e349ee89833a76df017433ba5717f844ee992c
Andy Brown <andy@techdad.tech>
2021-12-30 18:02:36 +0000

    a twitter joke related to annie and peegs

---
+++ wholikespeegs/wholikespeegs.py
@@ -0,0 +1,14 @@
+
+
+
+def main():
+    print("Enter a thing.....")
+    thing = input("...")
+    if thing == "peegs":
+        print("Annie would like this....")
+    else:
+        print("Annie may like this, more research needed....")
+
+
+if __name__ == '__main__':
+    main()
